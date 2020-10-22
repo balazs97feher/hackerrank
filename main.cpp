e55ec86ec405cf4ac54b165f3d303793dca1aa25
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2020-10-22 16:05:12 +0200

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,61 @@
+#include <fstream>
+#include <limits>
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
-int main() {
-    cout << "hello" << endl;
+// Complete the quickestWayUp function below.
+int quickestWayUp(vector<vector<int>> ladders, vector<vector<int>> snakes) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        vector<vector<int>> ladders(n);
+        for (int i = 0; i < n; i++) {
+            ladders[i].resize(2);
+
+            for (int j = 0; j < 2; j++) {
+                cin >> ladders[i][j];
+            }
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        int m;
+        cin >> m;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        vector<vector<int>> snakes(m);
+        for (int i = 0; i < m; i++) {
+            snakes[i].resize(2);
+
+            for (int j = 0; j < 2; j++) {
+                cin >> snakes[i][j];
+            }
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        int result = quickestWayUp(ladders, snakes);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
 
     return 0;
 }
