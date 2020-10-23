82ff16a59cab05768264afe043bf5bf05cd735fb
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2020-10-23 21:23:25 +0200

    Add option to read from file/console

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
+#include <fstream>
 #include <iostream>
+#include <memory>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "hello" << endl;
+class Reader {
+   public:
+    Reader() {}
+    virtual string readLine() = 0;
+};
+
+class FileReader : public Reader {
+    std::ifstream inputFile;
+
+   public:
+    FileReader(const string fileName) : Reader() {
+        inputFile = ifstream(fileName);
+    }
+    string readLine() override {
+        string line;
+        getline(inputFile, line);
+        return line;
+    }
+};
+
+class ConsoleReader : public Reader {
+   public:
+    ConsoleReader() : Reader() {}
+    string readLine() override {
+        string line;
+        getline(cin, line);
+        return line;
+    }
+};
+
+main(int argc, char *argv[]) {
+    unique_ptr<Reader> reader;
+    if (argc == 2)
+        reader = unique_ptr<FileReader>(new FileReader(argv[1]));
+    else
+        reader = unique_ptr<ConsoleReader>(new ConsoleReader);
 
     return 0;
 }
