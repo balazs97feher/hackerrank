34ce558078dc07dc3bd2d7c6527cca61bdd81af8
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2020-10-24 09:26:43 +0200

    Read input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -35,12 +37,35 @@ class ConsoleReader : public Reader {
     }
 };
 
+struct Edge {
+    int from, to, weight;
+    Edge(int f, int t, int w) : from(f), to(t), weight(w) {}
+};
+
 main(int argc, char *argv[]) {
     unique_ptr<Reader> reader;
     if (argc == 2)
         reader = unique_ptr<FileReader>(new FileReader(argv[1]));
     else
         reader = unique_ptr<ConsoleReader>(new ConsoleReader);
+
+    int n, m;
+    stringstream ss{reader->readLine()};
+    ss >> n >> m;
+
+    vector<Edge> edges;
+    edges.reserve(m);
+
+    for (int i = 0; i < m; i++) {
+        ss = stringstream{reader->readLine()};
+        int from, to, weight;
+        ss >> from >> to >> weight;
+        edges.emplace_back(from, to, weight);
+    }
+
+    int r;
+    ss = stringstream{reader->readLine()};
+    ss >> r;
 
     return 0;
 }
