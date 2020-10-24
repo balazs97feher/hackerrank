3aad231ce2f39c7152d46d1f4deb9517bf9e2777
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2020-10-24 14:19:15 +0200

    Implement prims sample test case ok

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include <algorithm>
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <limits>
 #include <memory>
+#include <queue>
 #include <sstream>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -38,11 +43,64 @@ class ConsoleReader : public Reader {
 };
 
 struct Edge {
-    int from, to, weight;
-    Edge(int f, int t, int w) : from(f), to(t), weight(w) {}
+    int id, from, to;
+    uint32_t weight;
+    Edge(int id, int f, int t, uint32_t w) : id(id), from(f), to(t), weight(w) {}
+    Edge() = default;
 };
 
-main(int argc, char *argv[]) {
+vector<vector<int>> edgeList(int n, vector<Edge> &edges) {
+    vector<vector<int>> eList(n);
+
+    for (int i = 0; i < edges.size(); i++) {
+        eList[edges[i].from].push_back(i);
+        eList[edges[i].to].push_back(i);
+    }
+
+    return eList;
+}
+
+int otherEnd(int vertex, Edge &edge) {
+    if (edge.from == vertex) return edge.to;
+    return edge.from;
+}
+
+uint64_t prims(int n, int r, vector<Edge> &edges) {
+    vector<vector<int>> eList = edgeList(n, edges);
+    unordered_set<int> spanningTreeVertices;
+    unordered_set<int> spanningTreeEdges;
+    uint64_t MSTCost = 0;
+
+    auto compare = [](const Edge &lhs, const Edge &rhs) { return lhs.weight > rhs.weight; };
+    priority_queue<Edge, vector<Edge>, decltype(compare)> connectingEdges(compare);
+
+    int nextNode = r;
+    spanningTreeVertices.insert(nextNode);
+
+    while (spanningTreeVertices.size() != n) {
+        for (auto edgeId : eList[nextNode])
+            if (spanningTreeEdges.find(edgeId) == spanningTreeEdges.end()) connectingEdges.push(edges[edgeId]);
+
+        Edge cheapestEdge;
+        do {
+            cheapestEdge = connectingEdges.top();
+            connectingEdges.pop();
+        } while (spanningTreeVertices.find(cheapestEdge.from) != spanningTreeVertices.end() &&
+                 spanningTreeVertices.find(cheapestEdge.to) != spanningTreeVertices.end());
+
+        // cout << "popped :" << cheapestEdge.from << ' ' << cheapestEdge.to << ' ' << cheapestEdge.weight << endl;
+        spanningTreeEdges.insert(cheapestEdge.id);
+
+        MSTCost += cheapestEdge.weight;
+
+        nextNode = (spanningTreeVertices.find(cheapestEdge.from) == spanningTreeVertices.end()) ? cheapestEdge.from : cheapestEdge.to;
+        spanningTreeVertices.insert(nextNode);
+    }
+
+    return MSTCost;
+}
+
+int main(int argc, char *argv[]) {
     unique_ptr<Reader> reader;
     if (argc == 2)
         reader = unique_ptr<FileReader>(new FileReader(argv[1]));
@@ -58,14 +116,18 @@ main(int argc, char *argv[]) {
 
     for (int i = 0; i < m; i++) {
         ss = stringstream{reader->readLine()};
-        int from, to, weight;
+        int from, to;
+        uint32_t weight;
         ss >> from >> to >> weight;
-        edges.emplace_back(from, to, weight);
+        edges.emplace_back(i, from - 1, to - 1, weight);
     }
 
     int r;
     ss = stringstream{reader->readLine()};
     ss >> r;
+
+    auto result = prims(n, r - 1, edges);
+    cout << result;
 
     return 0;
 }
