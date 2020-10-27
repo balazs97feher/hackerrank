34675251c9e505957d2992224d1e7b3fa9b961c2
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2020-10-27 09:13:28 +0100

    Collect astronauts from the same country

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <memory>
 #include <sstream>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -41,6 +43,8 @@ class ConsoleReader : public Reader {
 
 using namespace reader;
 
+list<unordered_set<uint32_t>>::iterator findAstronaut(uint32_t astronaut, list<unordered_set<uint32_t>> &countries);
+
 int main(int argc, char *argv[]) {
     unique_ptr<Reader> reader;
     if (argc == 2)
@@ -48,5 +52,33 @@ int main(int argc, char *argv[]) {
     else
         reader = unique_ptr<ConsoleReader>(new ConsoleReader);
 
+    stringstream stream{reader->readLine()};
+    int n, p;
+    stream >> n >> p;
+
+    list<unordered_set<uint32_t>> countries;
+
+    for (int i = 0; i < p; i++) {
+        stream.str(reader->readLine());
+        uint32_t astronaut1, astronaut2;
+        stream >> astronaut1 >> astronaut2;
+
+        auto country = findAstronaut(astronaut1, countries);
+        if (country == countries.end()) country = findAstronaut(astronaut2, countries);
+        if (country == countries.end()) {
+            countries.emplace_back(unordered_set<uint32_t>{astronaut1, astronaut2});
+        } else {
+            country->insert({astronaut1, astronaut2});
+        }
+    }
+
+    
     return 0;
+}
+
+list<unordered_set<uint32_t>>::iterator findAstronaut(uint32_t astronaut, list<unordered_set<uint32_t>> &countries) {
+    for (auto country = countries.begin(); country != countries.end(); country++)
+        if (country->find(astronaut) != country->end()) return country;
+
+    return countries.end();
 }
