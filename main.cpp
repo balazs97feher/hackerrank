0427562296f942a6d91ec04a37b6fae60360c9bf
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2020-11-02 19:03:10 +0100

    Adjust upper bound

--- main.cpp
+++ main.cpp
@@ -53,8 +53,10 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-// Turan's theorem: graph with nodeCount vertices, free of K_cliqueSize
+// Turan's theorem: graph with nodeCount vertices, free of K_cliqueSize_+_1
 // Maximum number of edges
 uint32_t turansUpperBound(uint32_t nodeCount, uint32_t cliqueSize) {
-    return (cliqueSize - 2) / (2 * cliqueSize - 2) * nodeCount * nodeCount;
+    uint32_t r = nodeCount % cliqueSize;
+    return (cliqueSize - 1) / (2 * cliqueSize) * nodeCount * nodeCount -
+           (r * (cliqueSize - r) / (2 * cliqueSize));
 }
