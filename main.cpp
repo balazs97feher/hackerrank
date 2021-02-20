95feecea7de11776f13e76027dc6e076c9653236
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2021-02-20 18:27:56 +0100

    Solve this dude

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <sstream>
 #include <string>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -37,12 +38,43 @@ class ConsoleReader : public Reader {
     }
 };
 
+vector<int32_t> cache;
+vector<int16_t> numbers;
+
+uint32_t maxSumUntil(const int n)
+{
+    if (cache[n] != -1) return cache[n];
+    if (n == 1) return max<int32_t>(numbers[0], 0);
+    if (n == 2) return max<int32_t>(numbers[1], maxSumUntil(1));
+
+    int32_t maxSum = max<int32_t>(numbers[n - 1] + maxSumUntil(n - 2), maxSumUntil(n - 1));
+    if (maxSum >= 0) cache[n] = maxSum;
+    return maxSum;
+}
+
 int main(int argc, char *argv[]) {
     unique_ptr<Reader> reader;
     if (argc == 2)
         reader = unique_ptr<FileReader>(new FileReader(argv[1]));
     else
         reader = unique_ptr<ConsoleReader>(new ConsoleReader);
+
+    stringstream ss{ reader->readLine() };
+    int n;
+    ss >> n;
+
+    numbers.resize(n);
+    cache.resize(n+1);
+
+    ss = stringstream{ reader->readLine() };
+    for (int i = 0; i < n; i++)
+    {
+        ss >> numbers[i];
+        cache[i] = -1;
+    }
+    cache[n] = -1;
+
+    cout << maxSumUntil(n);
 
     return 0;
 }
