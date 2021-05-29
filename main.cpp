8170f4f345b577cad772aa2b2cea12a672b065b6
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2021-05-29 16:33:20 +0200

    Implement solution with unordered sets

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <list>
+#include <unordered_set>
 
 using namespace std;
 
 class Reader {
-   public:
+public:
     Reader() = default;
     virtual string readLine() = 0;
 };
@@ -16,7 +18,7 @@ class Reader {
 class FileReader : public Reader {
     ifstream inputFile;
 
-   public:
+public:
     FileReader(const string fileName) : Reader() {
         inputFile = ifstream(fileName);
     }
@@ -28,7 +30,7 @@ class FileReader : public Reader {
 };
 
 class ConsoleReader : public Reader {
-   public:
+public:
     ConsoleReader() = default;
     string readLine() override {
         string line;
@@ -37,12 +39,168 @@ class ConsoleReader : public Reader {
     }
 };
 
-int main(int argc, char *argv[]) {
+auto findSet(list<unordered_set<uint16_t>>& sets, const uint16_t s) {
+    for (auto it = sets.begin(); it != sets.end(); it++) {
+        if (it->find(s) != it->end()) return it;
+    }
+
+    return sets.end();
+}
+
+void store(list<unordered_set<uint16_t>>& sets, const uint16_t s, const uint16_t e) {
+    auto setOfS = findSet(sets, s);
+    auto setOfE = findSet(sets, e);
+
+    if (setOfE == sets.end() && setOfS == sets.end()) {
+        sets.push_back({ s, e });
+    }
+    else if (setOfE == sets.end()) {
+        setOfS->insert(e);
+    }
+    else if (setOfS == sets.end()) {
+        setOfE->insert(s);
+    }
+    else if (setOfS != setOfE) {
+        for (const auto elem : *setOfE) setOfS->insert(elem);
+        sets.erase(setOfE);
+    }
+}
+
+int main(int argc, char* argv[]) {
     unique_ptr<Reader> reader;
     if (argc == 2)
         reader = unique_ptr<FileReader>(new FileReader(argv[1]));
     else
         reader = unique_ptr<ConsoleReader>(new ConsoleReader);
+
+    stringstream stream{ reader->readLine() };
+
+    uint16_t n;
+    stream >> n;
+
+    list<unordered_set<uint16_t>> sets;
+
+    while (n-- > 0) {
+        uint16_t s, e;
+        stream = stringstream{ reader->readLine() };
+        stream >> s >> e;
+
+        store(sets, s, e);
+    }
+
+    uint16_t min = 30000;
+    uint16_t max = 0;
+
+    for (const auto& s : sets) {
+        const auto setSize = s.size();
+        if (setSize > max) max = setSize;
+        if (setSize < min && setSize > 1) min = setSize;
+    }
+
+    cout << min << ' ' << max << endl;
+
+    return 0;
+}
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <unordered_set>
+
+using namespace std;
+
+class Reader {
+public:
+    Reader() = default;
+    virtual string readLine() = 0;
+};
+
+class FileReader : public Reader {
+    ifstream inputFile;
+
+public:
+    FileReader(const string fileName) : Reader() {
+        inputFile = ifstream(fileName);
+    }
+    string readLine() override {
+        string line;
+        getline(inputFile, line);
+        return line;
+    }
+};
+
+class ConsoleReader : public Reader {
+public:
+    ConsoleReader() = default;
+    string readLine() override {
+        string line;
+        getline(cin, line);
+        return line;
+    }
+};
+
+auto findSet(list<unordered_set<uint16_t>>& sets, const uint16_t s) {
+    for (auto it = sets.begin(); it != sets.end(); it++) {
+        if (it->find(s) != it->end()) return it;
+    }
+
+    return sets.end();
+}
+
+void store(list<unordered_set<uint16_t>>& sets, const uint16_t s, const uint16_t e) {
+    auto setOfS = findSet(sets, s);
+    auto setOfE = findSet(sets, e);
+
+    if (setOfE == sets.end() && setOfS == sets.end()) {
+        sets.push_back({ s, e });
+    }
+    else if (setOfE == sets.end()) {
+        setOfS->insert(e);
+    }
+    else if (setOfS == sets.end()) {
+        setOfE->insert(s);
+    }
+    else if (setOfS != setOfE) {
+        for (const auto elem : *setOfE) setOfS->insert(elem);
+        sets.erase(setOfE);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    unique_ptr<Reader> reader;
+    if (argc == 2)
+        reader = unique_ptr<FileReader>(new FileReader(argv[1]));
+    else
+        reader = unique_ptr<ConsoleReader>(new ConsoleReader);
+
+    stringstream stream{ reader->readLine() };
+
+    uint16_t n;
+    stream >> n;
+
+    list<unordered_set<uint16_t>> sets;
+
+    while (n-- > 0) {
+        uint16_t s, e;
+        stream = stringstream{ reader->readLine() };
+        stream >> s >> e;
+
+        store(sets, s, e);
+    }
+
+    uint16_t min = 30000;
+    uint16_t max = 0;
+
+    for (const auto& s : sets) {
+        const auto setSize = s.size();
+        if (setSize > max) max = setSize;
+        if (setSize < min && setSize > 1) min = setSize;
+    }
+
+    cout << min << ' ' << max << endl;
 
     return 0;
 }
