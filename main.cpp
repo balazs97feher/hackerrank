cc9642ad5fb0bced92ef09b2a154d80ca873ffe9
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2021-06-08 21:07:37 +0200

    Implement solution

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -37,12 +38,56 @@ class ConsoleReader : public Reader {
     }
 };
 
-int main(int argc, char *argv[]) {
+bool abbreviation(const string &a, const string &b) {
+    if (b.size() == 1) {
+        if(a.size() < 1) return false;
+        int count = 0;
+        for (const auto c : a) if (isupper(c)) {
+            count++;
+            if (count == 2) return false;
+        }
+
+        for (size_t i = 0; i < a.size(); i++) {
+            if (toupper(a[i]) == b[0]) {
+                string tmp{ a };
+                tmp.erase(i, 1);
+
+                bool allLower = true;
+                for (const auto c : tmp) if (isupper(c)) {
+                    allLower = false;
+                    break;
+                }
+                if (allLower) return true;
+            }
+        }
+
+        return false;
+    }
+
+    for (size_t i = 0; i < a.size() - b.size() + 1; i++) {
+        if (toupper(a[i]) != b[0] && isupper(a[i])) return false;
+        if (toupper(a[i]) == b[0] && abbreviation(a.substr(i + 1), b.substr(1))) return true;
+    }
+
+    return false;
+}
+
+int main(int argc, char* argv[]) {
     unique_ptr<Reader> reader;
     if (argc == 2)
         reader = unique_ptr<FileReader>(new FileReader(argv[1]));
     else
         reader = unique_ptr<ConsoleReader>(new ConsoleReader);
+
+    int n;
+    stringstream stream{ reader->readLine() };
+    stream >> n;
+
+    while (n--) {
+        const string a = reader->readLine();
+        const string b = reader->readLine();
+        cout << (abbreviation(a, b) ? "YES" : "NO") << endl;
+    }
 
     return 0;
 }
