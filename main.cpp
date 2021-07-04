d36d2dc63c57988a5852e7c82229eb8f0a8aef44
FBalazs <balazs97feher@gmail.com>
2021-07-04 14:57:12 +0200

    Implement solution

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 class Reader {
-   public:
+public:
     Reader() = default;
     virtual string readLine() = 0;
 };
@@ -16,7 +17,7 @@ class Reader {
 class FileReader : public Reader {
     ifstream inputFile;
 
-   public:
+public:
     FileReader(const string fileName) : Reader() {
         inputFile = ifstream(fileName);
     }
@@ -28,7 +29,7 @@ class FileReader : public Reader {
 };
 
 class ConsoleReader : public Reader {
-   public:
+public:
     ConsoleReader() = default;
     string readLine() override {
         string line;
@@ -37,12 +38,39 @@ class ConsoleReader : public Reader {
     }
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     unique_ptr<Reader> reader;
     if (argc == 2)
         reader = unique_ptr<FileReader>(new FileReader(argv[1]));
     else
         reader = unique_ptr<ConsoleReader>(new ConsoleReader);
+
+    int n, k;
+    stringstream stream{ reader->readLine() };
+    stream >> n >> k;
+
+    stream = stringstream{ reader->readLine() };
+
+    priority_queue<uint64_t, vector<uint64_t>, greater<uint64_t>> candies;
+
+    while (n--) {
+        int c;
+        stream >> c;
+        candies.push(c);
+    }
+
+    int iter = 0;
+    while (candies.top() < k && candies.size() >= 2) {
+        int c1 = candies.top();
+        candies.pop();
+        int c2 = candies.top();
+        candies.pop();
+
+        candies.push(c1 + c2 + c2);
+        iter++;
+    }
+
+    cout << ((candies.top() >= k) ? iter : -1);
 
     return 0;
 }
