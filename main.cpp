8406569f73121ea5d3265138a173119bddb4ee4f
FBalazs <balazs97feher@gmail.com>
2021-10-02 15:58:55 +0200

    Implement solution with residue classes

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Reader {
-   public:
+public:
     Reader() = default;
     virtual string readLine() = 0;
 };
@@ -16,7 +17,7 @@ class Reader {
 class FileReader : public Reader {
     ifstream inputFile;
 
-   public:
+public:
     FileReader(const string fileName) : Reader() {
         inputFile = ifstream(fileName);
     }
@@ -28,7 +29,7 @@ class FileReader : public Reader {
 };
 
 class ConsoleReader : public Reader {
-   public:
+public:
     ConsoleReader() = default;
     string readLine() override {
         string line;
@@ -37,12 +38,49 @@ class ConsoleReader : public Reader {
     }
 };
 
-int main(int argc, char *argv[]) {
+[[ nodiscard ]] uint32_t maxNonDivisibleSubset(const vector<uint32_t> numbers, const uint32_t k) {
+    if (k == 1) return 0;
+
+    vector< uint32_t> classes(k, 0);
+    for_each(numbers.begin(), numbers.end(), [&classes, k](const uint32_t n) {
+        classes[n % k]++;
+        });
+
+    if (k == 2) return (classes[0] > 0) + (classes[1] > 0);
+
+    uint32_t maxSize = 0;
+    if (classes[0] > 0) maxSize++;
+    if (k % 2 == 0 && classes[k / 2] > 0) maxSize++;
+
+    const size_t until = (k % 2 == 0) ? size_t(k / 2) - 1 : size_t(k / 2);
+
+    for (size_t i = 1; i <= until; i++) {
+        maxSize += max(classes[i], classes[k - i]);
+    }
+
+    return maxSize;
+}
+
+int main(int argc, char* argv[]) {
     unique_ptr<Reader> reader;
     if (argc == 2)
         reader = unique_ptr<FileReader>(new FileReader(argv[1]));
     else
         reader = unique_ptr<ConsoleReader>(new ConsoleReader);
+
+    uint32_t n, k;
+
+    stringstream stream{ reader->readLine() };
+    stream >> n >> k;
+
+    vector<uint32_t> numbers(n, 0);
+
+    stream = stringstream{ reader->readLine() };
+    for (auto& n : numbers) {
+        stream >> n;
+    }
+
+    cout << maxNonDivisibleSubset(numbers, k) << endl;
 
     return 0;
 }
